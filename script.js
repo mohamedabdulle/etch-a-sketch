44f73815e59acc57eb3ac747272fc309616be7b8
mohamedabdulle <25498272+mohamedabdulle@users.noreply.github.com>
2023-04-14 21:01:40 -0400

    allow sketching, color picker, erasing sketches, and resetting canvas

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
+const initialGrid = 16;
 const slider = document.querySelector("#range");
 const gridCount = document.querySelector('#grid');
 const sketchCanvas = document.querySelector('#sketch');
+const style = document.querySelector('style');
+const sketchButtonColor = document.querySelector('#sketch-color');
+const canvasButtonColor = document.querySelector('#canvas-color');
+const eraser = document.querySelector('#eraser');
+const resetCanvas = document.querySelector('#reset-canvas');
+let sketchColor = sketchButtonColor.value;
+let canvasColor = canvasButtonColor.value;
+let previousBackgroundColor = canvasColor;
+let isMouseDown = false;
+let isDrag = false;
 
-(function() {
-  const counter = (gridCount.dataset.grid) ** 2;
-  for (let i = 0; i < counter; i++) {
-    sketchCanvas.appendChild(document.createElement('div'));
-  }
-})();
 
-slider.addEventListener('input', function(e) {
+sketchCanvas.onmouseover = function(event) {
+  let targetStyle = event.target.style;
+  previousBackgroundColor = targetStyle.backgroundColor;
+  targetStyle.backgroundColor = sketchColor;
+  targetStyle.border = "1px solid black";
+};
+
+sketchCanvas.onmouseout = function(event) {
+  let targetStyle = event.target.style;
+  targetStyle.border = "";
+  (isMouseDown || isDrag) ? targetStyle.backgroundColor = sketchColor : targetStyle.backgroundColor = previousBackgroundColor;
+  //If the cursor is not being currently dragged, then the grid box the cursor moved out of will be the last one colored.
+  isMouseDown = isDrag;
+};
+
+sketchCanvas.onmouseleave = () => {
+  isMouseDown = false;
+  isDrag = false;
+};
+
+sketchCanvas.onmousedown = function(event) {
+  isMouseDown = true;
+  isDrag = true;
+  event.target.style.backgroundColor = sketchColor;
+};
+
+sketchCanvas.onmouseup = () => isDrag = false;
+
+slider.oninput = generateGrids;
+slider.onmouseup = function() { style.innerHTML = newGridSize(this.value) };
+slider.onkeyup = function() { style.innerHTML = newGridSize(this.value) };
+ 
+canvasButtonColor.oninput = function() { 
+  canvasColor = this.value;
+  sketchColor = canvasColor;
+}
+
+sketchButtonColor.oninput = function() { sketchColor = this.value; }
+
+eraser.onclick = function() { sketchColor = this.value; }
+
+resetCanvas.onclick = buildCanvas;
+
+function generateGrids() {
   const newGridSize = this.value;
   const oldGridAmount = (gridCount.dataset.grid) ** 2;
   const newGridAmount = (newGridSize) ** 2;
@@ -23,8 +71,29 @@ slider.addEventListener('input', function(e) {
 
   const counter = Math.abs(newGridAmount - oldGridAmount);
 
+  style.innerHTML = '#sketch div { border: 1px solid black; width: ' + `${100/newGridSize}%` + ';}';
+
   for (let i = 0; i < counter; i++) {
     divAction(sketchCanvas);
   }
-});
+}
 
+function newGridSize(gridSize) {
+  return '#sketch div { width: ' + `${100/gridSize}%` + ';}';
+}
+ 
+function buildCanvas() {
+  const counter = (initialGrid) ** 2;
+  gridCount.textContent = `${initialGrid} X ${initialGrid}`;
+  gridCount.dataset.grid = initialGrid;
+  slider.value = initialGrid;
+  style.innerHTML = newGridSize(initialGrid);
+
+  const initialCanvasDivs = [];
+  for (let i = 0; i < counter; i++) {
+    initialCanvasDivs[i] = document.createElement('div');
+  }
+  sketchCanvas.replaceChildren(...initialCanvasDivs)
+};
+
+buildCanvas()
